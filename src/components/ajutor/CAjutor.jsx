8249fc27cab334bf5acfeefe8fad3c6d02ac7dd4
Madalina <mglogovetan5@gmail.com>
2023-02-08 18:56:07 +0200

    Pagina de Ajutor (am adaugat o sectiune de intrebari)

--- src/components/ajutor/CAjutor.jsx
+++ src/components/ajutor/CAjutor.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import EmailIcon from '@mui/icons-material/Email'
+import InputAdornment from '@mui/material/InputAdornment'
+import Button from '@mui/material/Button'
 
 const CAjutor = () => {
   return (
@@ -108,6 +112,51 @@ const CAjutor = () => {
                 alta cetatenie sau fara cetatenie).
               </p>
             </div>
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <p className='font-mono text-2xl mx-20 my-10'>
+            Aveți alte întrebări? Noi suntem aici să vă ajutăm!
+          </p>
+          <form className='flex flex-col max-w-lg mx-36'>
+            <TextField
+              maxWidth={400}
+              type={'email'}
+              variant='outlined'
+              placeholder='Emailul Tău'
+              autoComplete='email'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <EmailIcon />
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <br />
+            <TextField
+              id='outlined-multiline-static'
+              multiline
+              rows={4}
+              maxWidth={400}
+              type={'text'}
+              variant='outlined'
+              placeholder='Mesajul'
+              autoComplete='mesajul'
+            ></TextField>
+            <br />
+          </form>
+          <div className='mx-36'>
+            <Button
+              type='submit'
+              sx={{ borderRadius: 1 }}
+              variant='contained'
+              color='secondary'
+              fontFamily='sans-serif'
+              size='large'
+            >
+              Trimite
+            </Button>
           </div>
         </div>
       </Box>
